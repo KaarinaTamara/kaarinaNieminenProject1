e7bd1a5c8ecfb02566c357c9fd9453b9457731dd
Kaarina <kaarina.angus@gmail.com>
2021-07-06 22:42:38 -0400

    starting assignment2

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggle () {
+let menu = document.getElementById("hamburgerMenu");   
+
+if (menu.style.display === "block") {
+    menu.style.display = "none"
+    }
+    
+    else {
+        menu.style.display = "block"; 
+    }
+}
