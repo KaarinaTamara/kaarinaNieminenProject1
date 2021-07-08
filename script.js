1f0979ce5e0c4c591679735078dea105aefde54e
Kaarina <kaarina.angus@gmail.com>
2021-07-07 22:12:34 -0400

    almost done now

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// HAMBURGER MENU WAS CREATED WITH ASSISTANCE FROM CODING COMMANDERS AT https://www.youtube.com/watch?v=QMuIy72W5qk 
+
+
 function toggle () {
 let menu = document.getElementById("hamburgerMenu");   
 
@@ -9,3 +12,5 @@ if (menu.style.display === "block") {
         menu.style.display = "block"; 
     }
 }
+
+// document.getElementById("burgerButton").addEventListener("click",toggle)
